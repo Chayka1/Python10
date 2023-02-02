16f9bee704ea791058b0c64e16788a6729d914c5
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-02-02 15:46:27 +0200

    1

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-
-
+import sqlite3
 
 
 if __name__ == '__main__':
-    pass
+    conn = sqlite3.connect('Chinook_Sqlite.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM Artist")
+    results = cursor.fetchall()
+    print(results)
+    conn.close()
 
 
 
