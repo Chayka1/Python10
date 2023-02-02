6893467bb1532a52c1895bcd0bc8dd6735ff8767
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-02-02 16:05:45 +0200

    1

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 import sqlite3
 
 
-if __name__ == '__main__':
-    conn = sqlite3.connect('Chinook_Sqlite.sqlite')
-    cursor = conn.cursor()
+class Album:
+    def __init__(self, album_id, title, artist_id):
+        self.artist_id = artist_id
+        self.title = title
+        self.album_id = album_id
 
-    cursor.execute("SELECT * FROM Artist")
-    results = cursor.fetchall()
-    print(results)
-    conn.close()
+    def __str__(self) -> str:
+        return f'{self.album_id}, {self.title}, {self.artist_id}'
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+class AlbumDAO:
+    def __init__(self):
+        self.conn = sqlite3.connect('Chinook_Sqlite.sqlite')
+        self.cursor = self.conn.cursor()
+
+    def find_all(self):
+        self.album_list = []
+        self.cursor.execute("SELECT * FROM Album")
+        results = self.cursor.fetchall()
+        for row in results:
+            self.album_list.append(Album(row[0], row[1], row[2]))
+        return self.album_list
+
+    def close(self):
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    album_db = AlbumDAO()
+    print(album_db.find_all())
+    album_db.close()
+
+
+
+
+
+
 
 
 
